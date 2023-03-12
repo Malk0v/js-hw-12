3e237b0a9be0d83eefd7486e94521037b3e7dc40
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-03-12 18:52:46 +0200

    Update news.js

--- src/js/news.js
+++ src/js/news.js
@@ -1,32 +1,40 @@
-// import NewsApiService from "./components/load-more";
+import LoadMoreBtn from "./components/load-more"
+import NewsApiService from "./components/news-service";
 
-// const refs = {
-//   searchForm: document.querySelector('.js-search-form'),
-//   articlesContainer: document.querySelector('.js-articles-container'),
-//   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-// };
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  articlesContainer: document.querySelector('.js-articles-container'),
+};
 
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
 
-// console.log(refs.searchForm)
-// console.log(refs.articlesContainer)
-// console.log(refs.loadMoreBtn);
+const newsApiService = new NewsApiService();
 
-// const newsApiService = new NewsApiService();
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
 
-// console.log(newsApiService);
+function onSearch(e) {
+    e.preventDefault();
 
-// refs.searchForm.addEventListener('submit', onSearch);
-// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+    newsApiService.query = e.currentTarget.elements.query.value;
+    
+    if (newsApiService.query === '') {
+        return alert('Введите что нормальное');
+    }
 
+    loadMoreBtn.show();
+    newsApiService.resetPage();
+    clearArticlesContainer();
+    fetchArticles();
+}
 
-// function onSearch(e) {
-//   e.preventDefault();
-
-//   newsApiService.query = e.currentTarget.elements.query.value;
-//   newsApiService.resetPage();
-//   newsApiService.fetchArticles();
-// }
-
-// function onLoadMore() {
-//   newsApiService.fetchArticles();
-// }
+function fetchArticles() {
+    loadMoreBtn.disabled();
+    newsApiService.fetchArticles().then(articles => {
+        appendArticlesMarup(articles);
+        loadMoreBtn.enable();
+    })
+}
