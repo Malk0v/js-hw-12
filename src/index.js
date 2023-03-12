f9b87f4cd24b14c521cb422292daf3d3b8b75891
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-03-12 18:52:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-// console.log('hello')
-
-// import poke from './templates/card.hbs';
-
-// console.log(poke);
-
-// const b = fetch(`https://pokeapi.co/api/v2/pokemon/1`).then(response => {
-//   return response.json();
-// });
-
-// const c = b.then(poke).catch(error => console.log());
-
-// console.log(c)
-
-// const f = poke({ description: "lasplspa" })
-
-// console.log(f)
-
+console.log('hello world');
